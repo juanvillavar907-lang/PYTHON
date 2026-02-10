24b8269110ffa840c4ce724775ee202d5ac48966
VJUAN <juanvillavar907@gmail.com>
2026-02-09 21:54:03 -0500

    Actualizando mi código con Git en Zed

---
+++ main.py
@@ -0,0 +1,17 @@
+def division_segura(a, b):
+    try:
+        resultado = a / b
+        return f"Resultado: {resultado}"
+    except ZeroDivisionError:
+        return "Error: No puedes dividir por cero, ¡matemáticas básicas!"
+
+
+print("--- Mi aprendizaje en Zed ---")
+num = 10
+den = 0
+
+
+print(division_segura(num, den))
+
+
+# Es una prueba de zed simple + git nada importante la verdad xd
